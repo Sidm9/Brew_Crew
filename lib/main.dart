3b690c93517ffb7bd7fa618b1dfbbbaaff4a84e2
Sidm9 <siddharthm1999@outlook.com>
2020-11-27 13:09:16 +0530

    Wrapped whole Stream by provider pkg to USER class

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:brew_crew/modals/user.dart';
 import 'package:brew_crew/screens/authenticate/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      // Stream provider "WRAPS" this up with the user Function <Stream> one
+      // and gives value weather user is signed in or not
+
+      // NOW EVERY CHILD CAN ACCESS THE DATA PROVIDED BY THE STREAM
+
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/authenticate/wrapper.dart
+++ lib/screens/authenticate/wrapper.dart
@@ -1,12 +1,16 @@
 // import 'package:brew_crew/home/home.dart';
+import 'package:brew_crew/modals/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return home or auth screen
 
+    final user = Provider.of<User>(context);
+    print(user);
     return Authenticate();
   }
 }
